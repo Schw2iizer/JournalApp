68d8e5dedc24657e6ab82d573c7cc08a31a1e452
Jeff Telford <jefftelford@gmail.com>
2015-03-10 12:08:10 -0600

    updated sweet alert text

--- public/js/controllers/registerController.js
+++ public/js/controllers/registerController.js
@@ -4,7 +4,7 @@ app.controller("registerController", function(registerService, $scope){
 		$scope.clickToRegister = function(){
 		registerService.register($scope.email, $scope.password)
 			.then(function(){
-				swal("You successfully registered!", "Thanks!", "success")
+				swal("You successfully registered!", "Now Please Login..", "success")
 			}, function(err){
 				swal("Oops...", "Something went wrong!", "error");
 			})
